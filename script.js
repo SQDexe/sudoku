f5d008d2a86218528aecf740a27b7ffaeec24f46
Jakub Szuchniewicz <66796000+SQDexe@users.noreply.github.com>
2023-06-30 00:19:21 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 class Sudoku {
     constructor(symbols = "123456789", size = 9) {
         /* 1234 - ♠♣♦♥ - nswe - 0123456789abcdef - abcdefghijklmnopqrstuvwxyz */
-        symbols = new Set(symbols.toString().split("")), size = Number(size);
-        symbols = Array.from(symbols);
-        if (typeof symbols !== "string") symbols = "123456789";
-        if (!Number.isInteger(size) || size < 0) size = 9;
+        symbols = Array.from(new Set(symbols.toString().split(""))), size = Number(size);
+        if (typeof symbols !== "string")
+            symbols = "123456789";
+        if (!Number.isInteger(size) || size < 0)
+            size = 9;
 
         size = size <= symbols.length ? size : symbols.length;
         this.sqrtSize = Math.floor(Math.sqrt(size));
@@ -12,12 +13,15 @@ class Sudoku {
         this.symbols = symbols.slice(0, this.size).split("");
         this.holes = 0.5;
         this.sudoku = []
-        for (let i = 0; i < this.size; i++) this.sudoku[i] = [];
-        for (let i = 0, l = this.size * this.size; i < l; i++) this.sudoku[Math.floor(i / this.size)][i % this.size] = null;
+        for (let i = 0; i < this.size; i++)
+            this.sudoku[i] = [];
+        for (let i = 0, l = this.size * this.size; i < l; i++)
+            this.sudoku[Math.floor(i / this.size)][i % this.size] = null;
         this.makeFull();
         }
     makeFull() {
-        for (let i = 0; i < this.sqrtSize; i++) this.generateBox(i);
+        for (let i = 0; i < this.sqrtSize; i++)
+            this.generateBox(i);
         this.generateRest(0, this.sqrtSize);
         this.generateHoles();
         }
@@ -27,17 +31,21 @@ class Sudoku {
             this.sudoku[x * this.sqrtSize + Math.floor(i / this.sqrtSize)][x * this.sqrtSize + (i % this.sqrtSize)] = symbols2Use.pop();
         }
     generateRest(x, y) {
-        if (x == this.size - 1 && y == this.size) return true;
+        if (x == this.size - 1 && y == this.size)
+            return true;
         if (y == this.size) {
             x++;
             y = 0;
             }
-        if (typeof this.sudoku[x][y] === "string") return this.generateRest(x, y + 1);
+        if (typeof this.sudoku[x][y] === "string")
+            return this.generateRest(x, y + 1);
         for (let symbol of this.getShuffledSymbols()) {
             if (this.safe(x, y, symbol)) {
                 this.sudoku[x][y] = symbol;
-                if (this.generateRest(x, y + 1)) return true;
-                else this.sudoku[x][y] = null;
+                if (this.generateRest(x, y + 1))
+                    return true;
+                else
+                    this.sudoku[x][y] = null;
                 }
             }
         return false;
@@ -66,4 +74,67 @@ class Sudoku {
             }
         }
     }
-exitGame = () => window.location = "sqdexe.github.io";
+const variables = Object.seal({
+    sudoku: null,
+    sudokuArea: $("#sudoku-area"),
+    overlay: $("#overlay"),
+    infoBox: $("#info-box")
+    });
+exit = () => window.location = "https://sqdexe.github.io";
+colorText = () => {
+    let mess = $("#message"), txt = mess.text();
+    mess.empty();
+    for (let letter of txt)
+        mess.append($("<span></span>").text(letter));
+    }
+generatePuzzle = () => {
+    variables.sudokuArea.empty();
+    variables.sudoku = new Sudoku();
+    let table = $("<table></table>"), bigRow = bigCell = row = cell = input = null;
+    for (let x = 0, y = i = j = rowNum = colNum = boxNum = null; x < variables.sudoku.sqrtSize; x++) {
+        bigRow = $("<tr></tr>");
+        for (y = 0; y < variables.sudoku.sqrtSize; y++) {
+            boxNum = x * variables.sudoku.sqrtSize + y % variables.sudoku.sqrtSize;
+            bigCell = $("<td></td>");
+            bigCell.append($("<table></table>").attr("id", "b" + boxNum));
+            for (i = 0; i < variables.sudoku.sqrtSize; i++) {
+                row = $("<tr></tr>");
+                for (j = 0; j < variables.sudoku.sqrtSize; j++) {
+                    rowNum = x * variables.sudoku.sqrtSize + i;
+                    colNum = y * variables.sudoku.sqrtSize + j;
+                    input = $("<input>").attr({
+                        "type": "text",
+                        "maxlength": "1",
+                        "value": ""
+                        });
+                    input.addClass("field").addClass("r" + rowNum).addClass("c" + colNum).addClass("b" + boxNum);
+                    if (variables.sudoku.sudoku[rowNum][colNum] != null)
+                        input.attr({
+                            "value": variables.sudoku.sudoku[rowNum][colNum],
+                            "readonly": ""
+                            });
+                    row.append($("<td></td>").append(input));
+                    }
+                bigCell.children().append(row);
+                }
+            bigRow.append(bigCell);
+            }
+        table.append(bigRow);
+        }
+    variables.sudokuArea.append(table);
+    }
+check = () => {
+    let full = $(".field").filter(function(i) {$(this).val() == ""}).length == 0;
+    if (full) {console.log($(".field").filter(function(i) {$(this).val() == ""}).length)}
+
+    }
+load = () => {
+    variables.overlay.addClass("hidden");
+    variables.infoBox.addClass("hidden");
+    colorText();
+    generatePuzzle();
+    $(".play").click(generatePuzzle);
+    $(".check").click(check);
+    $(".end").click(exit);
+    }
+$(document).ready(load);
